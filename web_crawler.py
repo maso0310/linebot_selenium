35849d3cd42b7f189029ac920654f1944d0f1f47
Maso <balimon@hotmail.com>
2021-09-12 12:20:37 +0800

    'ok'

--- web_crawler.py
+++ web_crawler.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from linebot.models import *
 from flex_msg import *
 from config import *
@@ -129,3 +128,4 @@ if __name__=='__main__':
     message = youtube_vedio_parser('YT,Maso的萬事屋')
     line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
     line_bot_api.push_message(USERID,message)
+    
